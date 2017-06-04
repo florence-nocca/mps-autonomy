122623a808cbdb2420c96ec8ad4f7d11ee9c50ee
Florence Nocca <florence.nocca@yahoo.fr>
2017-06-04 11:02:47 +0200

    Add source of the code

--- retrieve-tweets/url_exists.sh
+++ retrieve-tweets/url_exists.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for url in $(cat datafiles/italian_urls) ; do
+for url in $(cat datafiles/spanish_urls) ; do
     if curl --output /dev/null --silent --head --fail "$url"
     then
 	echo "$url exists"
@@ -8,3 +8,5 @@ for url in $(cat datafiles/italian_urls) ; do
 	echo "$url does not exist"
     fi
 done
+
+## Code taken from Charles Duffy's answer: https://stackoverflow.com/questions/12199059/how-to-check-if-an-url-exists-with-the-shell-and-probably-curl
