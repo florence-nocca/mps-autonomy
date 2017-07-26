159224a875c6a967dcfe2b597705530e50d7904f
florence-nocca <florence.nocca@yahoo.fr>
2017-07-26 17:28:12 +0200

    Selection of parties in chi-test and multinomial model

--- data-analysis/5_explain_cand_scores.r
+++ data-analysis/5_explain_cand_scores.r
@@ -17,7 +17,17 @@ options(scipen=999)
 
 ## Read candidates data
 cand_data = read.csv("tweets/cand_scores.csv", header = TRUE, na.strings=c(""))
+
+
+## Round wordscores value
 cand_data$wordscores = round(cand_data$wordscores, digits = 1)
+cand_data$wordscores = round(cand_data$wordscores, digits = 0)
+
+
+## Select only candidates from main parties
+cand_data = cand_data[cand_data$nuance == "SOC" | cand_data$nuance == "LR" | cand_data$nuance == "REM" | cand_data$nuance == "UDI",]
+
+cand_data = droplevels(cand_data)
 
 ## --- Exploratory Analysis ---
 set.seed(1023)
@@ -43,6 +53,7 @@ cols_withNa = apply(cand_data_subset, 2, function(x) sum(is.na(x)))
 ## --- Categorical Variable Analysis ---
 factor_vars = names(which(sapply(cand_data_subset, class) == "factor"))
 factor_vars = setdiff(factor_vars, "account")
+factor_vars = setdiff(factor_vars, "wordscores")
 
 chisq_test_res = lapply(factor_vars, function(x) { 
     chisq.test(cand_data_subset[,x], cand_data_subset[, "wordscores"], simulate.p.value = TRUE)
@@ -62,5 +73,33 @@ barchart_res = lapply(factor_vars, function(x) {
 
 names(barchart_res) = factor_vars
 barchart_res$nuance
-barchart_res$naissance
-barchart_res$sex
+
+## Descriptive statistics
+table(cand_data_subset$wordscores, cand_data_subset$nuance)
+
+## Logistic regression
+training.data.raw = cand_data
+
+sapply(training.data.raw,function(x) sum(is.na(x)))
+
+sapply(training.data.raw, function(x) length(unique(x)))
+
+## Missing data
+library(Amelia)
+missmap(training.data.raw, main = "Missing values vs observed")
+
+## Removing variables with too many na
+data = subset(training.data.raw,select=-c(delu1,elue2))
+missmap(data, main = "Missing values vs observed")
+
+library(nnet)
+data$nuance = relevel(data$nuance, ref = "REM")
+mod = multinom(wordscores ~ nuance, data)
+predict(mod,data,"probs")
+
+## Odds (antilog the coeff with exp function)
+exp(coef(mod))
+
+library(stargazer)
+stargazer(mod, title="Regression", align=TRUE)
+
